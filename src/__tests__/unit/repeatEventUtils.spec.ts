cabb09bfba9e588be6770c5637bff68273c575b7
effozen <ashgrayblue0@gmail.com>
2025-02-13 22:13:42 +0900

    test : 반복일정이 있는 지에 대한 테스트 코드 작성

--- src/__tests__/unit/repeatEventUtils.spec.ts
+++ src/__tests__/unit/repeatEventUtils.spec.ts
@@ -1,9 +1,16 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 
 import { Event } from '../../types';
-import { generateDailyDates, shouldExpandEvent } from '../../utils/repeatEventUtils';
+import {
+  generateDailyDates,
+  generateWeeklyDates,
+  generateMonthlyDates,
+  generateYearlyDates,
+  generateRepeatingDates,
+  shouldExpandEvent,
+} from '../../utils/repeatEventUtils';
 
-describe('repeatEventUtils', () => {
+describe('repeatEventUtils >', () => {
   let baseEvent: Event;
 
   beforeEach(() => {
@@ -21,41 +28,19 @@ describe('repeatEventUtils', () => {
     };
   });
 
-  describe('shouldExpandEvent 테스트', () => {
-    it('반복 설정이 없으면 false 반환', () => {
+  describe('shouldExpandEvent >', () => {
+    it('반복 설정이 없으면 false 반환한다.', () => {
       expect(shouldExpandEvent(baseEvent)).toBe(false);
     });
 
-    it('반복 설정이 있고 endDate가 있으면 true 반환', () => {
+    it('반복 설정이 있고 endDate가 있으면 true 반환한다.', () => {
       baseEvent.repeat = { type: 'daily', interval: 1, endDate: '2025-01-10' };
       expect(shouldExpandEvent(baseEvent)).toBe(true);
     });
 
-    it('repeat.type은 있지만 endDate가 없으면 false 반환', () => {
+    it('repeat.type은 있지만 endDate가 없으면 false 반환한다.', () => {
       baseEvent.repeat = { type: 'daily', interval: 1, endDate: '' };
       expect(shouldExpandEvent(baseEvent)).toBe(false);
-    });
-  });
-
-  describe('매일 반복되는 일정 테스트', () => {
-    it('매일 반복', () => {
-      baseEvent.repeat = { type: 'daily', interval: 1, endDate: '2025-01-05' };
-      expect(generateDailyDates(baseEvent)).toEqual([
-        '2025-01-02',
-        '2025-01-03',
-        '2025-01-04',
-        '2025-01-05',
-      ]);
-    });
-
-    it('2일 간격 매일 반복', () => {
-      baseEvent.repeat = { type: 'daily', interval: 2, endDate: '2025-01-10' };
-      expect(generateDailyDates(baseEvent)).toEqual([
-        '2025-01-03',
-        '2025-01-05',
-        '2025-01-07',
-        '2025-01-09',
-      ]);
     });
   });
 });
